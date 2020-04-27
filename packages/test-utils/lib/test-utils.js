942a2afd6f89bac431e8995942058765626a0040
Tim Leslie <timl@thinkmill.com.au>
2020-04-27 15:01:24 +1000

    Consolidate connection URL API for adapters

--- packages/test-utils/lib/test-utils.js
+++ packages/test-utils/lib/test-utils.js
@@ -22,7 +22,7 @@ async function setupServer({
     mongoose: getMongoMemoryServerConfig,
     knex: () => ({
       dropDatabase: true,
-      knexOptions: { connection: process.env.KNEX_URI || 'postgres://localhost/keystone' },
+      url: process.env.DATABASE_URL || 'postgres://localhost/keystone',
     }),
   }[adapterName];
 
